f05e98190d9562d43e3e4fa6233b5fd584dce1c2
DanielBainbridge <contact.danielbainbridge@gmail.com>
2023-09-13 17:00:41 +1000

    Bug Fixes On Bugs

--- Assets/CODE/Combat/Enemy/EnemyBase.cs
+++ Assets/CODE/Combat/Enemy/EnemyBase.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Utility;
 
@@ -11,7 +12,7 @@ namespace Enemy
         [Rename("Aim Range")] public float f_aimRange = 6;
         [Rename("Fire Range")] public float f_fireRange = 4;
         [Rename("Melee Damage")] public float f_meleeDamage = 8;
-        [Rename("Melee Knockback")]
+        [Rename("Melee Knockback")] public float f_meleeKnockback = 3;
 
         //runtime variables
         private PlayerController C_player;
@@ -23,7 +24,7 @@ namespace Enemy
             SetRotationDirection(new Vector2(transform.forward.x, transform.forward.z));
         }
 
-        private void Update()
+        protected override void Update()
         {
             base.Update();
         }
@@ -51,19 +52,35 @@ namespace Enemy
 
         public void ReflectMovementDirection(Vector2 normal)
         {
-            ChangeMovementDirection(Vector2.Reflect(S_movementVec2Direction, normal));
+            ChangeMovementDirection(Vector2.Reflect(S_movementVec2Direction, normal).normalized);
         }
 
         protected override void Move()
         {
             base.Move();
         }
-        private void OnCollisionEnter(Collision collision)
+
+
+        protected virtual void MeleeDamage()
         {
-            PlayerController player = collision.transform.GetComponent<PlayerController>();
+            Collider[] collisions = Physics.OverlapSphere(transform.position, f_size * 1.95f);
+            PlayerController player = null;
+            for (int i = 0; i < collisions.Length; i++)
+            {
+                if (collisions[i].transform.GetComponent<PlayerController>() != null)
+                {
+                    player = collisions[i].transform.GetComponent<PlayerController>();
+                }
+            }
+
             if (player != null)
             {
-                player.Damage(f_meleeDamage);
+                if (player.e_combatState != CombatState.Invincible && player.e_combatState != CombatState.Dodge)
+                {
+                    player.Damage(f_meleeDamage);
+                    player.AddVelocity(new Vector3(DirectionOfPlayer().x, 0, DirectionOfPlayer().y) * f_meleeKnockback);
+                    return;
+                }
             }
         }
     }

04852861229216459849278426026a73b2bcaa77
DanielBainbridge <contact.danielbainbridge@gmail.com>
2023-10-09 17:02:38 +1100

    Gun Module UI Part 1

---
+++ Assets/CODE/Managers/UI/GunModuleCard.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Utility;
+using TMPro;
+using Gun;
+
+public class GunModuleCard : MonoBehaviour
+{
+    public CanvasGroup C_canvasGroup;
+    public GunModule C_gunModuleReference;
+    public GunModuleUIAnimations C_moduleAnimations;
+    [Rename("Gun Module Transform")] public Transform C_gunModuleTransform;
+    [Rename("Gun Module Name")] public TextMeshProUGUI C_gunModuleName;
+    [Rename("Module Stat 1 Name")] public TextMeshProUGUI C_moduleStat1Name;
+    [Rename("Module Stat 2 Name")] public TextMeshProUGUI C_moduleStat2Name;
+    [Rename("Module Stat 3 Name")] public TextMeshProUGUI C_moduleStat3Name;
+    [Rename("Module Stat 4 Name")] public TextMeshProUGUI C_moduleStat4Name;
+
+    [Rename("Stat 1 Value Bar")] public Image C_statValueBar1;
+    [Rename("Stat 2 Value Bar")] public Image C_statValueBar2;
+    [Rename("Stat 3 Value Bar")] public Image C_statValueBar3;
+    [Rename("Stat 4 String")] public TextMeshProUGUI C_statString;
+
+    public void UpdateGunModule(GunModule gunModuleToSet)
+    {
+        C_gunModuleReference = gunModuleToSet;
+        UpdateCardInfo();
+    }
+
+    private void UpdateCardInfo()
+    {
+        C_gunModuleName.text = C_gunModuleReference.s_moduleName;
+        switch (C_gunModuleReference.e_moduleType)
+        {
+            case GunModule.ModuleSection.Trigger:
+                //Stat Names
+                C_moduleStat1Name.text = "Damage";
+                C_moduleStat2Name.text = "Fire Rate";
+                C_moduleStat3Name.text = "Bullet Speed";
+                C_moduleStat4Name.text = "Bullet Type";
+                //Values scale on x axis for horizontal fill
+                C_statValueBar1.transform.localScale = new Vector3(ExtraMaths.Map(4, 35, 0.1f, 0.9f, C_gunModuleReference.f_baseDamage), 1, 1);
+                C_statValueBar2.transform.localScale = new Vector3(ExtraMaths.Map(1, 9, 0.1f, 0.9f, C_gunModuleReference.f_fireRate), 1, 1);
+                C_statValueBar3.transform.localScale = new Vector3(ExtraMaths.Map(12, 20, 0.1f, 0.9f, C_gunModuleReference.f_bulletSpeed), 1, 1);
+                C_statString.text = C_gunModuleReference.S_bulletTraitInformation.e_bulletTrait.ToString();
+                break;
+
+            case GunModule.ModuleSection.Clip:
+                //Stat Names
+                C_moduleStat1Name.text = "Clip Size";
+                C_moduleStat2Name.text = "Reload Time";
+                C_moduleStat3Name.text = "Weight";
+                C_moduleStat4Name.text = "Bullet Element";
+                //Values scale on x axis for horizontal fill
+                C_statValueBar1.transform.localScale = new Vector3(ExtraMaths.Map(6, 38, 0.1f, 0.9f, C_gunModuleReference.i_clipSize), 1, 1);
+                C_statValueBar2.transform.localScale = new Vector3(ExtraMaths.Map(0.6f, 2.5f, 0.1f, 0.9f, C_gunModuleReference.f_reloadSpeed), 1, 1);
+                C_statValueBar3.transform.localScale = new Vector3(ExtraMaths.Map(0, 2.25f, 0.1f, 0.9f, C_gunModuleReference.f_movementPenalty), 1, 1);
+                C_statString.text = C_gunModuleReference.S_bulletEffectInformation.e_bulletEffect.ToString();
+                break;
+
+            case GunModule.ModuleSection.Barrel:
+                //Stat Names
+                C_moduleStat1Name.text = "Bullet Size";
+                C_moduleStat2Name.text = "Range";
+                C_moduleStat3Name.text = "Recoil";
+                C_moduleStat4Name.text = "Shot Pattern";
+                //Values scale on x axis for horizontal fill
+                C_statValueBar1.transform.localScale = new Vector3(ExtraMaths.Map(0.25f, 0.3f, 0.1f, 0.9f, C_gunModuleReference.f_bulletSize), 1, 1);
+                C_statValueBar2.transform.localScale = new Vector3(ExtraMaths.Map(3.85f, 11, 0.1f, 0.9f, C_gunModuleReference.f_bulletRange), 1, 1);
+                C_statValueBar3.transform.localScale = new Vector3(ExtraMaths.Map(0, 8, 0.1f, 0.9f, C_gunModuleReference.f_recoil), 1, 1);
+                C_statString.text = C_gunModuleReference.S_shotPatternInformation.e_shotPattern.ToString();
+                break;
+        }
+    }
+    
+    public void Show()
+    {
+        C_canvasGroup.alpha = 1;
+    }
+
+    public void Fade()
+    {
+        StartCoroutine(FadeOverSeconds(0.2f));
+    }
+
+    private IEnumerator FadeOverSeconds(float seconds)
+    {
+        float startTime = Time.time;
+        while(Time.time - startTime < seconds)
+        {
+            C_canvasGroup.alpha = Mathf.Lerp(1, 0, (Time.time - startTime / seconds)); 
+            yield return 0;
+        }
+        yield return null;
+    }
+}
---
+++ Assets/CODE/Managers/UI/GunModuleUIAnimations.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Utility;
+using TMPro;
+using Gun;
+
+[RequireComponent(typeof(Animator))]
+public class GunModuleUIAnimations : MonoBehaviour
+{
+    [Rename("Animator")] Animator C_animator;
+
+    [Rename("Trigger Transform"), SerializeField] private Transform C_triggerTransform = null;
+    [Rename("Clip Transform"), SerializeField] private Transform C_clipTransform = null;
+    [Rename("Barrel Transform"), SerializeField] private Transform C_barrelTransform = null;
+    [Rename("Swapping Transform"), SerializeField] private Transform C_swappingTransform = null;
+
+    [Rename("Trigger Joint")] public Transform C_triggerJoint = null;
+    [Rename("Clip Joint")] public Transform C_clipJoint = null;
+    [Rename("Barrel Joint")] public Transform C_barrelJoint = null;
+    [Rename("Swapping Transform"), SerializeField] private Transform C_swappingJoint = null;
+
+
+
+    public void PlayPauseUI()
+    {
+        C_animator.SetFloat("Trigger", 0.0f);
+        C_animator.SetFloat("Clip", 0.0f);
+        C_animator.SetFloat("Barrel", 0.0f);
+        C_animator.Play("Default");
+    }
+    public void PlayTriggerIdle()
+    {
+        C_animator.SetFloat("Trigger", 1.0f);
+    }
+    public void PlayClipIdle()
+    {
+        C_animator.SetFloat("Clip", 1.0f);
+    }
+    public void PlayBarrelIdle()
+    {
+        C_animator.SetFloat("Barrel", 1.0f);
+    }
+    public void PlayTriggerSwap()
+    {
+        C_animator.SetFloat("Trigger", 2.0f);
+    }
+    public void PlayClipSwap()
+    {
+        C_animator.SetFloat("Clip", 2.0f);
+    }
+    public void PlayBarrelSwap()
+    {
+        C_animator.SetFloat("Barrel", 2.0f);
+    }
+
+    public void SwapTriggerMesh(GunModule gunModule)
+    {
+        Destroy(C_triggerTransform.gameObject);
+
+        C_triggerTransform = Instantiate(gunModule.C_meshPrefab).transform;
+        C_triggerTransform.gameObject.tag = "Player";
+        if (C_triggerTransform.GetComponent<Collider>() != null)
+        {
+            Destroy(C_triggerTransform.GetComponent<Collider>());
+        }
+
+        C_triggerTransform.parent = C_triggerJoint;
+        C_triggerTransform.localPosition = Vector3.zero;
+        C_triggerTransform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        C_triggerTransform.localScale = Vector3.one;
+    }
+    public void SwapClipMesh(GunModule gunModule)
+    {
+        Destroy(C_clipTransform.gameObject);
+
+        C_clipTransform = Instantiate(gunModule.C_meshPrefab).transform;
+        C_clipTransform.gameObject.tag = "Player";
+        if (C_clipTransform.GetComponent<Collider>() != null)
+        {
+            Destroy(C_clipTransform.GetComponent<Collider>());
+        }
+
+        C_clipTransform.parent = C_triggerJoint;
+        C_clipTransform.localPosition = Vector3.zero;
+        C_clipTransform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        C_clipTransform.localScale = Vector3.one;
+    }
+    public void SwapBarrelMesh(GunModule gunModule)
+    {
+        Destroy(C_barrelTransform.gameObject);
+
+        C_barrelTransform = Instantiate(gunModule.C_meshPrefab).transform;
+        C_barrelTransform.gameObject.tag = "Player";
+        if (C_barrelTransform.GetComponent<Collider>() != null)
+        {
+            Destroy(C_barrelTransform.GetComponent<Collider>());
+        }
+
+        C_barrelTransform.parent = C_triggerJoint;
+        C_barrelTransform.localPosition = Vector3.zero;
+        C_barrelTransform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        C_barrelTransform.localScale = Vector3.one;
+    }
+    public void SwapSwappingMesh(GunModule gunModule)
+    {
+        Destroy(C_swappingTransform.gameObject);
+
+        C_swappingTransform = Instantiate(gunModule.C_meshPrefab).transform;
+        C_swappingTransform.gameObject.tag = "Player";
+        if (C_swappingTransform.GetComponent<Collider>() != null)
+        {
+            Destroy(C_swappingTransform.GetComponent<Collider>());
+        }
+
+        C_swappingTransform.parent = C_swappingJoint;
+        C_swappingTransform.localPosition = Vector3.zero;
+        C_swappingTransform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        C_swappingTransform.localScale = Vector3.one;
+    }
+
+}
